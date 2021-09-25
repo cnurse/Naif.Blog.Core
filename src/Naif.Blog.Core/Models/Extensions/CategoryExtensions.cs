60039306dc9bdb36dfa8690e52d936d703032713
Charles Nurse <charlesnurse@gmail.com>
2021-09-25 12:16:40 -0700

    Add new ToString method for an IList<Category> Up version to 3.1.0-beta.4

--- src/Naif.Blog.Core/Models/Extensions/CategoryExtensions.cs
+++ src/Naif.Blog.Core/Models/Extensions/CategoryExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -5,6 +6,11 @@ namespace Naif.Blog.Models.Extensions
 {
     public static class CategoryExtensions
     {
+        public static string ToString(this IList<Category> categories, string separator)
+        {
+            return String.Join(separator, categories.Select(c => c.Name));
+        }
+
         public static string[] ToStringArray(this IList<Category> categories)
         {
             return categories.Select(c => c.Name).ToArray();
