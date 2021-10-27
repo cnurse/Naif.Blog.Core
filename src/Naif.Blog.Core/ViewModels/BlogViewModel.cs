03ebeb224facb6f8ecebbd65256ad4710ac8de2e
Charles Nurse <charlesnurse@gmail.com>
2021-10-27 15:12:46 -0700

    Move ViewModels from UI project to Core Up version to 3.1.2-alpha.1

---
+++ src/Naif.Blog.Core/ViewModels/BlogViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Naif.Blog.Models;
+using Naif.Core.Models;
+
+namespace Naif.Blog.ViewModels
+{
+    public class BlogViewModel
+    {
+        public string BaseUrl { get; set; }
+        
+        public Naif.Blog.Models.Blog Blog { get; set; }
+        
+        public IList<Message> Messages { get; set; }
+        
+        public int PageIndex { get; set; }
+        
+        public IEnumerable<Post> Pages { get; set; }
+
+        public Post Post { get; set; }
+
+        public IEnumerable<Post> Posts { get; set; }
+        
+        public string ReturnUrl { get; set; }
+        
+        public User User { get; set; }
+    }
+}
---
+++ src/Naif.Blog.Core/ViewModels/EditPostViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Naif.Core.Models;
+
+namespace Naif.Blog.ViewModels
+{
+    public class EditPostViewModel
+    {
+        public string ActionUrl { get; set; }
+        
+        public IList<Message> Messages { get; set; }
+
+        public int PageIndex { get; set; }
+
+        public PostViewModel Post { get; set; }
+
+        public List<SelectListItem> Pages { get; set; }
+                
+        public string ReturnUrl { get; set; }
+    }
+}
